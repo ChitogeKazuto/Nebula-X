c309cddb72ec86f27d849b8779eae7c4dd90445d
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-20 22:19:38 +0700

    Update

---
+++ Whitelist/BDMUK7xKtD.lua
@@ -0,0 +1,13 @@
+_G.Key = "BDMUK7xKtD"
+
+local Name = game:GetService("Players").LocalPlayer.Character.Name
+
+if _G.Key == "BDMUK7xKtD" then
+    if Name == "ChitogeKazuto" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Nebula-X/NebuleX/Nebule%20X.lua"))()
+    else
+        game:GetService("Players").LocalPlayer:kick("Unknow Username")
+        end
+    else
+        game:GetService("Players").LocalPlayer:kick("Unknow Key")
+end
