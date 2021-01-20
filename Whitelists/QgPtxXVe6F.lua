1c31e8c2059f79f7338ba45806cf08675a2a95e5
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-20 22:29:24 +0700

    Create QgPtxXVe6F.lua

---
+++ Whitelists/QgPtxXVe6F.lua
@@ -0,0 +1,11 @@
+local Name = game:GetService("Players").LocalPlayer.Character.Name
+
+if _G.Key == "QgPtxXVe6F" then
+    if Name == "poommegeggthai" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Nebula-X/NebuleX/Nebule%20X.lua"))()
+    else
+        game:GetService("Players").LocalPlayer:kick("Unknow Username")
+        end
+    else
+        game:GetService("Players").LocalPlayer:kick("Unknow Key")
+end
