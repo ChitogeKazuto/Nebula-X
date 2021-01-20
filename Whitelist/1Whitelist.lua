505d774f33da353852bfc6c7e2baffeca60b72a9
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-20 12:20:34 +0700

    Update

--- Whitelist/1Whitelist.lua
+++ Whitelist/1Whitelist.lua
@@ -4,7 +4,7 @@ local Key = game:HttpGet("https://pastebin.com/raw/dkY8Rnbf", true)
 local Banned = game:HttpGet("https://pastebin.com/raw/j9KYxFHG", true)
 
 if string.find(Key, _G.Key) then
-    print("Yes, I did it")
+    game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Nebula-X/NebuleX/Nebule%20X.lua", true)
 else
-    game:GetService("Players").LocalPlayer:kick("LOL")
+    game:GetService("Players").LocalPlayer:kick("Nebule X : UnWhitelist")
 end
